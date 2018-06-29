9c78c268129e7e8285e15b232afa0ce8af91c869
mounika27021995 <siddulamounika@gmail.com>
2018-06-29 11:51:17 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world! Welcome to JS';
+
+var myBtn = document.querySelector('button');
+
+myBtn.onclick = function(){
+    alert("Hello World!");
+}
